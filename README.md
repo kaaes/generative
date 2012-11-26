d6ee892f54c843a8b5bea0af1ddabdf3e27c3359
Kasia <kasia@drzyzga.pl>
2012-11-26 01:06:41 +0100

    Sparks! And some cleaning

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-generative-flowers
+Generative
 ==================
+
+Canvas playground:
+
+* [Sparks & Wires](http://kaaes.github.com/generative/sparks.html)
+* [Flowers, cookies, atoms & what not](http://kaaes.github.com/generative/flowers.html)
+* [Random circles](http://kaaes.github.com/generative/circles.html)
+* [Really long curve](http://kaaes.github.com/generative/curves.html)
--- particle.js
+++ particle.js
@@ -1,14 +1,17 @@
-function Particle() {
-	if (typeof Vector !== 'function') {
-		throw new Error('Vector is necessary for Particle well being!');
-	}
+function Particle(x, y) {
+  if (typeof Vector !== 'function') {
+    throw new Error('Vector is necessary for Particle well being!');
+  }
 
-  this.position = new Vector();
+  this.position = new Vector(x, y);
   this.velocity = new Vector();
-  this.acceleration = new Vector();
+  //this.acceleration = new Vector();
 
   this.radius = 1;
+  this.mass = 1;
 }
+
+Particle.G = 1;
 
 Particle.prototype.setPosition = function(x, y) {
   this.position.x = x;
@@ -20,11 +23,18 @@ Particle.prototype.setVelocity = function(x, y) {
   this.velocity.y = y;
 }
 
+Particle.prototype.addForce = function(force) {
+  this.velocity.add(force);
+
+  if (this.velocity.magnitude() > 25) {
+    this.velocity.div(2);
+  }
+}
+
 Particle.prototype.update = function(ctx) {
-  this.velocity.add(this.acceleration);
   this.position.add(this.velocity);
 
-  this.draw(ctx);
+  //this.draw(ctx);
 
   // if (this.position.x + this.radius * 0.5 >= window.innerWidth) {
   //   this.velocity.x *= -1;
@@ -48,8 +58,21 @@ Particle.prototype.draw = function(ctx) {
   ctx.fillStyle = 'rgba(243,91,82,.9)';
   ctx.translate(this.position.x, this.position.y);
   ctx.beginPath();
-  ctx.arc(0, 0, this.radius, 0, Math.PI * 2);
+  ctx.arc(0, 0, this.mass / 2, 0, Math.PI * 2);
   ctx.closePath();
   ctx.fill();
   ctx.restore();
+}
+
+Particle.prototype.attract = function(mover) {
+  var force = Vector.sub(this.position, mover.position);
+  var distance = force.magnitude();
+
+  distance = distance < 5 ? 5 : distance > 25 ? 25 : distance;
+
+  force.normalize();
+  var strength = (Particle.G * this.mass * mover.mass) / (distance * distance);
+  force.mult(strength);
+
+  return force;
 }
