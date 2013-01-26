be73ccc3a5a1788b6e03c334806c408e166ef7b4
Kasia <kasia@drzyzga.pl>
2013-01-27 00:32:50 +0100

    Remove unnecessary file

--- utils.js
+++
@@ -1,9 +0,0 @@
-var utils = {
-  random: function(min, max) {
-    return min + Math.floor(Math.random() * (max - min + 1));
-  },
-
-  rad: function(angle) {
-    return angle * Math.PI/180;
-  }
-}
